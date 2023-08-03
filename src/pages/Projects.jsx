99aa54a32dbad10407dd4546371ec0f248540f14
Lawmake Praveen <lawmakepraveen00@gmail.com>
2023-08-03 21:03:02 +0530

    projects information icon

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Project from "./components/Project";
 import { motion, useScroll, useSpring } from "framer-motion";
 import { projectsArray } from "./components/ProjectContent";
+import { HiInformationCircle } from "react-icons/hi";
 
 const Projects = () => {
   const { scrollYProgress } = useScroll();
@@ -16,30 +17,46 @@ const Projects = () => {
   window.addEventListener("resize", () => {
     setWindowWidth(window.innerWidth < 600);
   });
+  const [showDisclaimer, setShowDisclaimer] = useState(false);
 
   return (
     <>
       {windowWidth && (
         <motion.div style={{ scaleX }} className="scroll-progress"></motion.div>
       )}
-
       <motion.div
         className="projects"
         initial={{ opacity: 0, x: "-100%" }}
         animate={{ opacity: 1, x: 0 }}
         exit={{ opacity: 0, x: "100%" }}
       >
-        {projectsArray.map((project, index) => {
-          return (
-            <Project
-              key={index}
-              content={project.content}
-              link={project.link}
-              appLink={project.appLink}
-              image={project.image}
-            />
-          );
-        })}
+        <div className="projects-disclaimer">
+          <span className={`disclaimer ${showDisclaimer && 'show-dis'}`}>
+            All these projects have been completely designed and developed by
+            me, without any external help.
+          </span>
+          <span
+            className="diclaimer-icon"
+            onMouseEnter={() => setShowDisclaimer(true)}
+            onMouseLeave={() => setShowDisclaimer(false)}
+          >
+            <HiInformationCircle />
+          </span>
+        </div>
+
+        <div className="all-projects">
+          {projectsArray.map((project, index) => {
+            return (
+              <Project
+                key={index}
+                content={project.content}
+                link={project.link}
+                appLink={project.appLink}
+                image={project.image}
+              />
+            );
+          })}
+        </div>
       </motion.div>
     </>
   );
