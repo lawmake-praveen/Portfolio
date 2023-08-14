562368d7d7397f45255c45d19effc0f5510b2989
Lawmake Praveen <lawmakepraveen00@gmail.com>
2023-08-14 20:33:26 +0530

    minor changes

--- src/assets/Project-screenshot/export.js
+++ src/assets/Project-screenshot/export.js
@@ -6,6 +6,6 @@ export { default as tictactoe } from "./tic-tac-toe.png";
 export { default as todo } from "./todo-app.png";
 export { default as v2painting } from "./v2-painting.jpg";
 export { default as weather } from "./weather.jpg";
-export { default as portfolio } from "./portfolio.jpg";
+export { default as portfolio } from "./portfolio.png";
 export { default as shopease } from "./shopease.jpg";
 export { default as activities } from './activities-tab.png'
